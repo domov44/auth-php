311d72773f3373743d8c64a44f80fcce3812011b
Ronan Scotet <ronanscotet467@gmail.com>
2023-11-17 18:33:59 +0100

    logout feature

--- index.php
+++ index.php
@@ -5,6 +5,20 @@ if (!isLoggedIn()) {
     header("Location: login.php");
     exit();
 }
-
-echo "Bienvenue sur la page protégée!";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page protégée</title>
+</head>
+<body>
+    <h2>Bienvenue sur la page protégée!</h2>
+    
+    <form method="post" action="logout.php"> <!-- Utilisez l'action pour pointer vers logout.php -->
+        <input type="submit" name="logout" value="Déconnexion">
+    </form>
+</body>
+</html>
